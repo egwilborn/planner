f0bd3dfc2fb52fc257e7c34dcffbf78e3db0159b
egwilborn <egwilborn@gmail.com>
2023-09-13 10:58:59 -0500

    shows add list content form

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('lists/create/', views.ListCreate.as_view(), name="list_create"),
     path('lists/', views.ListIndex.as_view(), name="list_index"),
+    path('lists/<int:pk>', views.ListDetail.as_view(), name="list_detail"),
+    path('lists/<int:list_id>/todos/create',
+         views.todo_create, name="create_todo")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .forms import TodoForm
 
 
 from .models import List
@@ -19,3 +20,16 @@ class ListCreate(CreateView):
 
 class ListIndex(ListView):
     model = List
+
+
+class ListDetail(DetailView):
+    model = List
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_form"] = TodoForm
+        return context
+
+
+def todo_create(request, list_id):
+    render()
